83de6698961023f1f362c1c76b4faa5117074c73
malenarivera <89429614+malenarivera@users.noreply.github.com>
2021-10-20 22:23:11 -0300

    Update S4.java
    y este??????????? MEJORADO TAMBIEN

--- S4.java
+++ S4.java
@@ -5,37 +5,23 @@
  */
 package concurrenteTeoria;
 
-import java.util.concurrent.Semaphore;
 /**
  *
  * @author Diana
  */
 public class S4 implements Runnable{
-    private int w;
-    private S3 sem3;
-    private Semaphore s3_s4;
-    private Semaphore s4;
+    Proceso p;
     
-    public S4 (S3 sem3){
-        this.s4=new Semaphore(1);
-        
-        this.sem3=sem3;
+    public S4 (Proceso p){
+        this.p=p;
     }
     
     public void operarS4(){
-        w=sem3.getC()+1;
-        System.out.println("W: "+w);
+     p.calcularW();
     }    
        
     public void run(){
-        if(sem3.adquirirs3_s4()){
-      try{
-          s4.acquire();
             this.operarS4();
-        s4.release();
-      }catch(InterruptedException e){}
-    }
            
 }
 }
-
