3315d40ee515bb50bc640da4938c1f30e6352cdf
malenarivera <89429614+malenarivera@users.noreply.github.com>
2021-10-20 22:25:48 -0300

    Update mainSemaforosDePaso.java
    MEJORADO EL MAIN TMB PORQUE SI MEJORAMOS 1 SE MEJORA TODO

--- mainSemaforosDePaso.java
+++ mainSemaforosDePaso.java
@@ -6,7 +6,6 @@
 package concurrenteTeoria;
 
 import java.lang.Thread;
-import java.util.concurrent.Semaphore;
 
 /**
  *
@@ -14,26 +13,26 @@ import java.util.concurrent.Semaphore;
  */
 public class mainSemaforosDePaso {
     public static void main(String[] args) {
-    int x,y,z;
-    x=10;
-    y=11;
-    z=12;
+   Proceso p= new Proceso (1,2,3);
    
-    S1 s1= new S1(x,y);
-    S2 s2= new S2(z);
-    S3 s3= new S3(s1,s2);
-    S4 s4= new S4(s3);
+    S1 s1= new S1(p);
+    S2 s2= new S2(p);
+    S3 s3= new S3(p);
+    S4 s4= new S4(p);
     
     Thread sssem1= new Thread (s1);
     Thread sssem2= new Thread(s2);
     Thread sssem3= new Thread(s3);
-    Thread sssem4= new Thread(s4);
+    Thread sssem4= new Thread (s4);
     
     sssem1.start();
     sssem2.start();
-   sssem3.start();
-   sssem4.start();
+    sssem3.start();
+    sssem4.start();
+    
+    
     }
     
     
 }
+
