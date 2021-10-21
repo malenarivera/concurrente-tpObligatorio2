b72b3c57d58aa1854fe76d00402d1ff1fcc71732
malenarivera <89429614+malenarivera@users.noreply.github.com>
2021-10-20 22:22:24 -0300

    Update S3.java
    mejoradoooooooooooooooooooo

--- S3.java
+++ S3.java
@@ -5,55 +5,28 @@
  */
 package concurrenteTeoria;
 
-import java.util.concurrent.Semaphore;
-import java.lang.Thread;
 /**
  *
  * @author Diana
  */
 public class S3 implements Runnable {
-    private int c;
-private Semaphore s3_s4;
-private Semaphore s3;
-private S1 sem1;
-private S2 sem2;
+Proceso p;
 
-public S3 (S1 sem1,S2 sem2){
-    this.sem1=sem1;
-    this.sem2=sem2;
-    s3_s4= new Semaphore (0);
-    this.s3=new Semaphore(1);
-}
-
-public int getC(){
-return c;
+public S3 (Proceso p){
+    this.p=p;
 }
 
 
 public void operarS3(){
-      c=sem1.getA()-sem2.getB();
-       System.out.println("C: "+c);
+p.calcularC();
 }
   
-
-public boolean adquirirs3_s4(){
-    return (s3_s4.tryAcquire());
-}
-    
-
     public void run() {
-        if(sem1.adquirirs1_s3()&&sem2.adquirirs2_s3()){
-            try {
-                s3.acquire();
                 this.operarS3();
-                s3_s4.release();
-            } catch (InterruptedException e) {
-            }
         }
         
         
     }
-}
 
     
 
