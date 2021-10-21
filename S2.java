90e64cc98948718e00f1d7a8c13e16650c4a39c0
malenarivera <89429614+malenarivera@users.noreply.github.com>
2021-10-20 22:21:37 -0300

    Update S2.java
    mejorado ;)

--- S2.java
+++ S2.java
@@ -5,40 +5,23 @@
  */
 package concurrenteTeoria;
 
-import java.util.concurrent.Semaphore;
 /**
  *
  * @author Diana
  */
 public class S2 implements Runnable {
-    private int b,z;
-    private Semaphore s2;
-    private Semaphore s2_s3;
+    Proceso p;
     
-    public S2( int z){
-        this.z=z;
-        this.s2=new Semaphore (1);
-        s2_s3= new Semaphore(0);
+    public S2( Proceso p){
+        this.p=p;
     }
     
     public void operarS2(){
-        b=z-1;
-        System.out.println("B: "+b);
+        p.calcularB();
     }
     
-     public boolean adquirirs2_s3(){
-        return (s2_s3.tryAcquire());
-    }
-    
-    public int getB(){
-        return b;
-    }
     
     public void run(){
-        try{
-            s2.acquire();
             this.operarS2();
-            s2_s3.release();
-    }catch(InterruptedException e){}
-   }
+}
 }
